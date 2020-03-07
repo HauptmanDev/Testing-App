19751ff42af33f3f6eff41ba3151aa91430842e5
ibulavsky <ibulavsky@gmail.com>
2020-03-07 10:04:32 +0300

    Added redux-dev-tools.

--- src/components/store/store.js
+++ src/components/store/store.js
@@ -5,7 +5,7 @@ const reducers = combineReducers({
     // signIn: signInReducer,
 });
 
-const store = createStore(reducers, applyMiddleware(thunkMiddleware));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunkMiddleware)));
 
 export default store
-
