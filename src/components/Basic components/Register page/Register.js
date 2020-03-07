2d2a0efa587da4edf8aa20e4dce1ed3ecbef0fda
HauptmanDev <MainVados@gmail.com>
2020-03-07 12:31:35 +0300

    Changed Register page.

--- src/components/Basic components/Register page/Register.js
+++ src/components/Basic components/Register page/Register.js
@@ -1,15 +1,37 @@
-import style from './Register.module.css';
-import React from 'react'
+import React from "react";
+import { useForm } from "react-hook-form";
 
+const RegisterPage = () => {
+    const { handleSubmit, register, errors } = useForm();
+    const onSubmit = values => {
+        console.log(values);
+    };
 
-function RegisterPage() {
     return (
-        <div className={style.menu}>
-            <span>Регистрация</span>
-            <span>Авторизация</span>
-            <span>Контакты</span>
-        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <input
+                name="email"
+                ref={register({
+                    required: 'Required',
+                    pattern: {
+                        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                        message: "invalid email address"
+                    }
+                })}
+            />
+            {errors.email && errors.email.message}
+
+            <input
+                name="username"
+                ref={register({
+                    validate: value => value !== "admin" || "Nice try!"
+                })}
+            />
+            {errors.username && errors.username.message}
+
+            <button type="submit">Submit</button>
+        </form>
     );
-}
+};
 
 export default RegisterPage;
