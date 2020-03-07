3abd5555483d8dffea15f7c960ae1229413705e4
HauptmanDev <MainVados@gmail.com>
2020-03-07 12:56:44 +0300

    Added new files.

--- src/components/Basic components/Register page/Register.js
+++ src/components/Basic components/Register page/Register.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import FormsWrapper from "./FormsWrapper";
 
 const RegisterPage = () => {
     const { handleSubmit, register, errors } = useForm();
@@ -8,29 +9,33 @@ const RegisterPage = () => {
     };
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <input
-                name="email"
-                ref={register({
-                    required: 'Required',
-                    pattern: {
-                        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                        message: "invalid email address"
-                    }
-                })}
-            />
-            {errors.email && errors.email.message}
+        <div>
+            <FormsWrapper>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <input
+                        name="email"
+                        ref={register({
+                            required: 'Required',
+                            pattern: {
+                                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                                message: "invalid email address"
+                            }
+                        })}
+                    />
+                    {errors.email && errors.email.message}
+                    <input
+                        name="username"
+                        ref={register({
+                            validate: value => value !== "admin" || "Nice try!"
+                        })}
+                    />
+                    {errors.username && errors.username.message}
+                    <button type="submit">Submit</button>
+                </form>
 
-            <input
-                name="username"
-                ref={register({
-                    validate: value => value !== "admin" || "Nice try!"
-                })}
-            />
-            {errors.username && errors.username.message}
+            </FormsWrapper>
+        </div>
 
-            <button type="submit">Submit</button>
-        </form>
     );
 };
 
