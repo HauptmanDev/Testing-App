bd51c116c80aab312df736be34425f15e8e5a291
HauptmanDev <MainVados@gmail.com>
2020-03-08 17:39:16 +0300

    Made minor adjustments.

--- src/Presentation_components/Basic components/Greeting page/Greeting.js
+++ src/Presentation_components/Basic components/Greeting page/Greeting.js
@@ -1,11 +1,14 @@
-import style from './Greeting.module.css';
+import styles from './Greeting.module.css';
 import React from 'react'
+import {NavLink} from "react-router-dom";
 
 
 function GreetingPage() {
     return (
-        <div className={style.menu}>
-            <span>Hello</span>
+        <div className={styles.greeting}>
+            <div className={styles.text}>
+                <span>Привет, новобранец! </span><span><NavLink to='/register'>Регистрируйся</NavLink> и проверь себя, готов ли ты стать САМУРАЕМ!!!</span>
+            </div>
         </div>
     );
 }
