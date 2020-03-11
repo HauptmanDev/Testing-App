0734c2f4fabd5f60a662552f32698a4a965ea17a
HauptmanDev <MainVados@gmail.com>
2020-03-11 22:53:02 +0300

    Made minor adjustments - changed Register Form.

--- src/Presentation_components/Basic components/Register page/RegisterForm.jsx
+++ src/Presentation_components/Basic components/Register page/RegisterForm.jsx
@@ -1,59 +1,68 @@
-import React from "react"
+import React, {useState} from "react"
 import {useForm} from "react-hook-form"
 import style from "./RegisterForm.module.scss"
 
 const RegisterForm = () => {
     const {handleSubmit, register, errors} = useForm();
+    const [dataObj, setDataPerson] = useState();
+    // const [successRegister, setStatusRegister] = useState(false);
     const onSubmit = values => {
-        console.log(values);
+        if (values.firstPassword !== values.secondPassword) {
+            alert('Пароли не совпадают')
+        } else {
+            console.log(values);
+            setDataPerson(values)
+        }
     };
 
     return (
-        <form onSubmit={handleSubmit(onSubmit)} className={style.form}>
-            <input className={style.input}
-                   name="email"
-                   placeholder={"эл. почта"}
-                   ref={register({
-                       required: 'Required',
-                       pattern: {
-                           value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                           message: "invalid email address"
-                       }
-                   })}
-            />
-            {errors.email && errors.email.message}
-            <input className={style.input}
-                   name="username"
-                   placeholder={"имя"}
-                   ref={register({
-                       required: 'Required',
-                       validate: value => value !== "admin" || "Nice try!"
-                   })}
-            />
-            {errors.username && errors.username.message}
-            <input className={style.input}
-                   name="firstPassword"
-                   placeholder={"пароль"}
-                   type={'password'}
-                   ref={register({
-                       required: 'Required',
-                       validate: value => value !== "admin" || "Nice try!"
-                   })}
-            />
-            {errors.firstPassword && errors.firstPassword.message}
-            <input className={style.input}
-                   name="secondPassword"
-                   placeholder={"повторите пароль"}
-                   type="password"
-                   ref={register({
-                       required: 'Required',
-                       validate: value => value !== "admin" || "Nice try!"
-                   })}
-            />
-            {errors.secondPassword && errors.secondPassword.message}
-            <button className={style.button} type="submit">Подтвердить</button>
-        </form>
+        <div>
+             <form onSubmit={handleSubmit(onSubmit)} className={style.form}>
+                <input className={style.input}
+                       name="email"
+                       placeholder={"Эл. почта"}
+                       ref={register({
+                           required: 'Required',
+                           pattern: {
+                               value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
+                               message: "invalid email address"
+                           }
+                       })}
+                />
+                {errors.email && errors.email.message}
+                <input className={style.input}
+                       name="username"
+                       placeholder={"Имя"}
+                       ref={register({
+                           required: 'Required',
+                           validate: value => value !== "admin" || "Nice try!",
+                       })}
+                />
+                {errors.username && errors.username.message}
+                <input className={style.input}
+                       name="firstPassword"
+                       placeholder={"Пароль"}
+                       type={'password'}
+                       ref={register({
+                           required: 'Required',
+                           validate: value => value !== "123" || "Ты серьезно?"
+                       })}
+                />
+                {errors.firstPassword && errors.firstPassword.message}
+                <input className={style.input}
+                       name="secondPassword"
+                       placeholder={"Повторите пароль"}
+                       type="password"
+                       ref={register({
+                           required: 'Required',
+                           // validate: value => value !== '123' || "Nice try!"
+                       })}
+                />
+                {errors.secondPassword && errors.secondPassword.message}
+                <button className={style.button} type="submit">Подтвердить</button>
+            </form>
+        </div>
     )
-}
+};
 
 export default RegisterForm;
